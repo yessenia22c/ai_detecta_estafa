cd9907119ff6b73028eb325fca747ef1051ae490
yessenia22c <yesseniavillarte@gmail.com>
2024-07-10 15:09:14 -0400

    refactor: Update ModelSelect component styles for better user experience

--- src/components/ModelSelect.tsx
+++ src/components/ModelSelect.tsx
@@ -13,7 +13,7 @@ export const ModelSelect: FC<Props> = ({ model, onChange }) => {
 
   return (
     <select
-      className="h-[40px] w-[140px] rounded-md bg-[#2363bc] px-4 py-2 text-neutral-200 active:bg-green-700"
+      className="h-[40px] w-[140px] rounded-md bg-[#404a58] px-4 py-2 text-neutral-200 "
       value={model}
       onChange={handleChange}
     >
