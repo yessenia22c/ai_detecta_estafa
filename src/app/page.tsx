bc1c812af944946897625476ee3a9f1af1fe7702
yessenia22c <yesseniavillarte@gmail.com>
2024-07-10 15:09:00 -0400

    feat: Add RadialChart component for displaying data visually

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { APIKeyInput } from "@/components/APIKeyInput";
 import { ModelSelect } from "@/components/ModelSelect";
 import { UrlInput } from "@/components/UrlInput";
+import { RadialChart } from "@/components/ui/RadialChart";
 import { OpenAIModel } from "@/types/types";
 import { useState } from "react";
 
@@ -37,29 +38,30 @@ export default function Home() {
   };
   return (
     <>
-      <div className="flex h-full min-h-screen flex-col items-center bg-[#102143] px-4 pb-20 text-neutral-200 sm:px-10">
+      <div className="flex h-full min-h-screen flex-col items-center bg-[#091224] px-4 pb-20 text-neutral-200 sm:px-10">
         <div className="flex flex-col items-center justify-between p-24">
           <APIKeyInput
             apiKey={apiKey}
             onChange={handleApiKeyChange}
           ></APIKeyInput>
-          <div className="p-6 flex flex-col">
+          <div className="p-6 flex flex-col items-center">
             <label htmlFor="Url"> Introduce la url a verficar</label>
             <UrlInput urlWeb={urlWeb} onChange={handleUrl}></UrlInput>
           </div>
           <div className="mt-2 flex items-center space-x-2">
-          <ModelSelect model={model} onChange={(value) => setModel(value)} />
+            <ModelSelect model={model} onChange={(value) => setModel(value)} />
 
-          <button
-            className="w-[140px] cursor-pointer rounded-md bg-green-600 px-4 py-2 font-bold hover:bg-green-500 active:bg-green-700"
-            onClick={() => handleVerification()}
-            disabled={loading}
-          >
-            {loading ? "Verificando..." : "Verificar"}
-          </button>
+            <button
+              className="w-[140px] cursor-pointer rounded-md bg-green-600 px-4 py-2 font-bold hover:bg-green-500 active:bg-green-700"
+              onClick={() => handleVerification()}
+              disabled={loading}
+            >
+              {loading ? "Verificando..." : "Verificar"}
+            </button>
+          </div>
+          
         </div>
-        </div>
-        
+        <RadialChart></RadialChart>
       </div>
     </>
   );
