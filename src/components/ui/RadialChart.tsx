f5c34be26d5c956e673ae1d3f330bd86fcdb39e7
yessenia22c <yesseniavillarte@gmail.com>
2024-07-10 15:09:45 -0400

    feat: Add RadialChart component for visualizing data
    The code changes introduce a new file, `RadialChart.tsx`, which contains the implementation of the `RadialChart` component. This component is used for displaying data visually in a radial bar chart format. It enhances the functionality and user experience of the application by providing a visually appealing way to represent data.

---
+++ src/components/ui/RadialChart.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [{ verificacion: "Web", estafa: 70, confiable: 30 }]
+
+const chartConfig = {
+  estafa: {
+    label: "Estafa %",
+    color: "hsl(var(--chart-1))",
+  },
+  confiable: {
+    label: "Confiable %",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig
+
+export function RadialChart() {
+  const totalVisitors = chartData[0].estafa + chartData[0].confiable
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Verificación de sitio</CardTitle>
+        <CardDescription>Resultados</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-1 items-center pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square w-full max-w-[250px]"
+        >
+          <RadialBarChart
+            data={chartData}
+            endAngle={180}
+            innerRadius={80}
+            outerRadius={130}
+          >
+            {/* <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            /> */}
+            <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle">
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) - 16}
+                          className="fill-foreground text-2xl font-bold"
+                        >
+                          {totalVisitors.toLocaleString()}%
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 4}
+                          className="fill-muted-foreground"
+                        >
+                          Escaneo completo
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </PolarRadiusAxis>
+            <RadialBar
+              dataKey="estafa"
+              stackId="a"
+              cornerRadius={5}
+              fill="var(--color-estafa)"
+              className="stroke-transparent stroke-2"
+            />
+            <RadialBar
+              dataKey="confiable"
+              fill="var(--color-confiable)"
+              stackId="a"
+              cornerRadius={5}
+              className="stroke-transparent stroke-2"
+            />
+          </RadialBarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+        Los resultados del escaneo del sitio web son: 
+        </div>
+        <div className="leading-none text-muted-foreground">
+            <div className="flex flex-row items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="hsl(var(--chart-1))"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/></svg>
+                <p className="py-2 mx-2">Estafa {chartData[0].estafa}% </p> 
+            </div>    
+            <div className="flex flex-row items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="hsl(var(--chart-2))"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/></svg>
+                <p className="py-2 mx-2">Confiable {chartData[0].confiable}% </p> 
+            </div>               
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
