86d4b8d8f47d47db685c62d1eb5f27127b481ce1
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-07-10 22:30:25 -0400

    feat: Update verification route to use OpenAI API for generating text

--- src/app/api/verification/route.ts
+++ src/app/api/verification/route.ts
@@ -1,18 +1,36 @@
-import { createOpenAI, openai } from '@ai-sdk/openai';
-import { StreamingTextResponse, streamText } from 'ai';
+import { VerificationUrl } from '@/types/types';
+import { scrapeWebsite } from '@/utils';
+import { createOpenAI } from '@ai-sdk/openai';
+import { generateText, StreamingTextResponse, streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
-export async function POST(req: Request) {
-    let  context  = "Verificar la autenticidad de una url" 
-    const { messages } = await req.json();
-    const prompt = createOpenAI()
-    const result = await streamText({
-        prompt: context,
-        model: openai('gpt-4-turbo'),
-        messages,
-    });
 
-    return result.toAIStreamResponse();
+export async function POST(req: Request) {
+    //console.log('EL BODY===????', req)
+    const body = await req.json();
+    const { url, model, apiKey } = body;
+
+    //const { url, model, apiKey } = await (req.json()) as VerificationUrl;
+
+    const openai = createOpenAI({
+        apiKey: apiKey || process.env.OPENAI_API_KEY,
+        compatibility: 'strict'
+    });
+    const context = await scrapeWebsite(url);
+    //console.log('CONTEXTOOOOOOOOOO', context)
+    const { text } = await generateText({
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${apiKey || process.env.OPENAI_API_KEY}`,
+            },
+            model: openai(model),
+            // system: "Eres un experto analista de sitios web fraudulentos",
+            prompt: context,
+            maxTokens: 650
+         });
+
+    return Response.json({ text });;
+
 }
