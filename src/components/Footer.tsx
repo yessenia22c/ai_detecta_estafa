dc88639162b0d7a57887698acdb62b5591144f45
yessenia22c <yesseniavillarte@gmail.com>
2024-07-17 09:35:38 -0400

    Refactor Footer component to use a Vercel template for inspiration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
             <p className='flex p-2 font-medium justify-center  text-[18px]'>AI Detecta estafas</p>
             <p className='text-[14px]'>Todos los resultados mostrados en este sitio son generados por una inteligencia artificial.</p>
             <p className='text-[14px]'>
-            Este proyecto se ha inspirado en algunos componentes del <a className='hover:text-green-600' href="https://vercel.com/templates/next.js/ai-code-translator" target="_blank" rel="noreferrer">Template Vercel</a>
+            Este proyecto se ha inspirado en algunos componentes de una <a className='hover:text-green-600' href="https://vercel.com/templates/next.js/ai-code-translator" target="_blank" rel="noreferrer">plantilla de Vercel</a>
             </p>
             <p className='text-[14px]'>Desarrollado por <a className='hover:text-green-600' href="https://www.linkedin.com/in/yessenia22/" target="_blank" rel="noreferrer">Yessenia Villarte</a></p>
         </div>
