2cbbcf56e1bc8fb5274e8094995574fa72fe129a
yessenia22c <yesseniavillarte@gmail.com>
2024-07-17 09:37:41 -0400

    refactor: Update URL input placeholder to include "https://"

--- src/components/UrlInput.tsx
+++ src/components/UrlInput.tsx
@@ -10,7 +10,7 @@ export const UrlInput : FC<Props> =({urlWeb, onChange}) => {
     <input
         className="sm:w-[280px] w-60 mt-1 h-[30px] mx-2  rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
         type="url"
-        placeholder="www.ejemplo.com"
+        placeholder="https://www.ejemplo.com"
         value={urlWeb}
         onChange={(e) => onChange(e.target.value)}
       />
